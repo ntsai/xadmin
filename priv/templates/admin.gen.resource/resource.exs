000d60ac7263bacae55214842e6612569b842243
sai <3030159@qq.com>
2016-08-30 09:43:40 +0800

    添加 遗忘的priv 目录, 增加 xadmin 中文 po 翻译文件

---
+++ priv/templates/admin.gen.resource/resource.exs
@@ -0,0 +1,7 @@
+defmodule <%= base %>.XAdmin.<%= resource %> do
+  use XAdmin.Register
+
+  register_resource <%= base %>.<%= resource %> do
+
+  end
+end
---
+++ priv/templates/admin.install/dashboard.exs
@@ -0,0 +1,15 @@
+defmodule <%= base %>.XAdmin.Dashboard do
+  use XAdmin.Register
+
+  register_page "Dashboard" do
+    menu priority: 1, label: "<%= title_txt %>"
+    content do
+      div ".blank_slate_container#dashboard_default_message" do
+        span ".blank_slate" do
+          span "<%= welcome_txt %>"
+          small "<%= add_txt %>"
+        end
+      end
+    end
+  end
+end
